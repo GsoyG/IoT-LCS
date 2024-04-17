e507f37f85d91c85e507772f42a1df8ee39dffe3
Gsoy <GsoyJanYo@iCloud.com>
2024-04-18 02:02:25 +0800

    添加前端设备颜色改变

--- LCS-Frontend/src/components/DeviceList.vue
+++ LCS-Frontend/src/components/DeviceList.vue
@@ -31,9 +31,17 @@
               <template v-slot:default="{ isActive }">
                 <v-card title="设备编辑">
                   <v-card-text>
+                    <v-row class="pb-8" justify="center">
+                      <v-col cols="auto"
+                        :style="`width: 100px; height: 100px; background: #${getConfigColor(deviceConfig)}; border-radius: 20px;`"></v-col>
+                    </v-row>
                     <v-slider min="1" max="254" step="1" v-model="deviceConfig.Dimmer" label="亮度"
                       :disabled="disabledEdit"
                       @end="uploadDeviceState(device, 'Dimmer', deviceConfig.Dimmer)"></v-slider>
+                    <v-slider min="0" max="254" step="1" v-model="deviceConfig.Hue" label="色调" :disabled="disabledEdit"
+                      @end="uploadDeviceColor(device, 'Hue', deviceConfig.Hue)"></v-slider>
+                    <v-slider min="0" max="254" step="1" v-model="deviceConfig.Sat" label="饱和" :disabled="disabledEdit"
+                      @end="uploadDeviceColor(device, 'Sat', deviceConfig.Sat)"></v-slider>
                   </v-card-text>
 
                   <v-card-actions>
@@ -113,6 +121,31 @@ function getStatusText(device) {
   return '已关闭';
 }
 
+function hueToRgb(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
+}
+function getConfigColor(deviceConfig) {
+  let h, s, l;
+  h = deviceConfig.Hue / 254;
+  s = 1;
+  l = 1 - deviceConfig.Sat / 254 / 2;
+
+  let r, g, b;
+  const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+  const p = 2 * l - q;
+
+  r = Math.round(hueToRgb(p, q, h + 1 / 3) * 255);
+  g = Math.round(hueToRgb(p, q, h) * 255);
+  b = Math.round(hueToRgb(p, q, h - 1 / 3) * 255);
+
+  return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
 async function fetchDeviceList() {
   disabledEdit.value = true;
   try {
@@ -154,6 +187,12 @@ async function uploadDeviceState(device, key, value, disabled = true) {
 
   if (disabled) disabledEdit.value = false;
   return false;
+}
+
+async function uploadDeviceColor(device, key, value) {
+  if (await uploadDeviceState(device, key, value)) {
+    device.RGB = getConfigColor(deviceConfig.value);
+  }
 }
 
 async function switchDevicePower(device) {
