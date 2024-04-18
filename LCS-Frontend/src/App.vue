16fcc6394bdca13ed44109e6803f04c889551379
Gsoy <GsoyJanYo@iCloud.com>
2024-04-18 21:45:20 +0800

    添加前端首页标题栏

--- LCS-Frontend/src/App.vue
+++ LCS-Frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <v-main>
+      <AppTitleBar/>
       <DeviceList/>
-      <!-- <ControlPanel /> -->
     </v-main>
   </v-app>
 </template>
---
+++ LCS-Frontend/src/components/AppTitleBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>智能照明控制系统</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-menu min-width="200px" rounded>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-avatar>
+              <v-img :alt="userData.user"
+                :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"></v-img>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-text>
+            <div class="mx-auto text-center">
+              <v-avatar size="80">
+                <v-img :alt="userData.user"
+                  :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"></v-img>
+              </v-avatar>
+              <h2 class="mt-2">{{ userData.user }}</h2>
+              <p class="mt-2">{{ userData.email }}</p>
+              <v-divider class="my-2"></v-divider>
+              <v-btn variant="text" rounded>
+                编辑信息
+              </v-btn>
+              <v-divider class="my-2"></v-divider>
+              <v-btn variant="text" rounded>
+                退出登录
+              </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const userData = ref({
+  'user': 'Gsoy',
+  'qq': '2754205302',
+  'email': 'GsoyG@icloud.com',
+});
+</script>
