e288e758f78260d21a4b69d04ff3bdce8c229a38
Gsoy <GsoyJanYo@iCloud.com>
2024-04-26 03:06:24 +0800

    优化前端登录代码

--- LCS-Frontend/src/Login.vue
+++ LCS-Frontend/src/Login.vue
@@ -9,6 +9,7 @@
             <v-card class="mx-auto pa-6" max-width="450" elevation="8">
               <p class="text-center text-h4 font-weight-thin mb-8 mt-4">智能照明控制系统</p>
 
+              <!-- 登入表单 -->
               <v-form v-model="data.form" @submit.prevent="login()">
                 <v-text-field label="用户名" v-model="data.username" :readonly="data.loading" :rules="[rules.username]"
                   placeholder="请输入用户名" clearable variant="outlined" prepend-icon="mdi-account" append-icon="none"
@@ -19,11 +20,13 @@
                   :type="rules.passwordShow ? 'text' : 'password'"
                   @click:append="rules.passwordShow = !rules.passwordShow"></v-text-field>
 
+                <!-- 登入提示信息 -->
                 <div class="text-center mt-2" v-if="tooltipConfig.show">
                   <v-icon :icon="tooltipConfig.icon" :color="tooltipConfig.iconColor" class="mr-2"></v-icon>
                   {{ tooltipConfig.text }}
                 </div>
-                <v-btn class="mt-4" text="登录" :disabled="!data.form" :loading="data.loading" color="indigo" size="large"
+                <!-- 登入按钮 -->
+                <v-btn class="mt-4" text="登入" :disabled="!data.form" :loading="data.loading" color="indigo" size="large"
                   type="submit" variant="elevated" block></v-btn>
               </v-form>
             </v-card>
@@ -38,19 +41,19 @@
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
 
-const data = ref({
+const data = ref({  // 登录数据
   'form': false,
   'username': null,
   'password': null,
   'loading': false,
 })
-const rules = ref({
+const rules = ref({ // 输入验证规则
   'username': value => !!value || '用户名不能为空',
   'password': value => !!value || '密码不能为空',
   'passwordMin': value => value.length >= 8 || '密码长度不能小于8位',
   'passwordShow': false
 })
-const tooltipConfig = ref({
+const tooltipConfig = ref({ // 提示文字信息
   'show': false,
   'text': '',
   'icon': '',
@@ -62,16 +65,16 @@ function showMessage(text, type) {
   switch (type) {
     case 'success':
       tooltipConfig.value = { show: true, text: text, icon: 'mdi-check-circle', iconColor: 'success' }
-      break;
+      break
     case 'warning':
       tooltipConfig.value = { show: true, text: text, icon: 'mdi-alert-circle', iconColor: 'warning' }
-      break;
+      break
     default:
-      break;
+      break
   }
 }
 
-// 登录
+// 登入
 async function login() {
   if (!data.value.form) return
 
@@ -80,13 +83,13 @@ async function login() {
     username: data.value.username,
     password: data.value.password
   }).then(response => {
-    showMessage('登录成功', 'success')
-    window.location.href = "http://127.0.0.1:3000/";
+    showMessage('登入成功', 'success')
+    window.location.href = "http://127.0.0.1:3000/"
   }).catch(error => {
     if (error.response)
-      showMessage('登录失败 ' + error.response.data, 'warning')
-    else showMessage('登录出错 ' + error.message, 'warning')
-  });
+      showMessage('登入失败 ' + error.response.data, 'warning')
+    else showMessage('登入出错 ' + error.message, 'warning')
+  })
   data.value.loading = false
 }
 
