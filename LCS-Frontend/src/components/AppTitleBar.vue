86b63049310dd760b1f984ed98de9eaaf109116a
Gsoy <GsoyJanYo@iCloud.com>
2024-04-26 16:25:38 +0800

    添加前端获取用户信息

--- LCS-Frontend/src/components/AppTitleBar.vue
+++ LCS-Frontend/src/components/AppTitleBar.vue
@@ -13,21 +13,23 @@
       <v-menu min-width="200px" rounded>
         <template v-slot:activator="{ props }">
           <v-btn icon v-bind="props">
-            <v-avatar>
-              <v-img :alt="userData.user"
-                :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"></v-img>
+            <v-avatar color="white">
+              <v-img :alt="userData.username" :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"
+                v-if="userData.qq != ''"></v-img>
+              <v-icon icon="mdi-account" color="indigo" size="large" v-else></v-icon>
             </v-avatar>
           </v-btn>
         </template>
         <v-card>
           <v-card-text>
             <div class="mx-auto text-center">
-              <v-avatar size="80">
-                <v-img :alt="userData.user"
-                  :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"></v-img>
+              <v-avatar color="white" size="80">
+                <v-img :alt="userData.username" :src="`http://q2.qlogo.cn/headimg_dl?dst_uin=${userData.qq}&spec=100`"
+                  v-if="userData.qq != ''"></v-img>
+                <v-icon icon="mdi-account" color="indigo" size="60" v-else></v-icon>
               </v-avatar>
-              <h2 class="mt-2">{{ userData.user }}</h2>
-              <p class="mt-2">{{ userData.email }}</p>
+              <h2 class="my-4">{{ userData.username }}</h2>
+              <p>{{ userData.email }}</p>
               <v-divider class="my-2"></v-divider>
               <v-btn text="编辑信息" variant="text" rounded></v-btn>
               <v-divider class="my-2"></v-divider>
@@ -39,13 +41,13 @@
     </template>
   </v-app-bar>
 
-  <!-- 左侧抽屉 桌面端 -->
-  <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false" v-if="!$vuetify.display.mobile">
+  <!-- 左侧抽屉 移动端 -->
+  <v-navigation-drawer v-model="rail" temporary @click="rail = false" v-if="$vuetify.display.mobile">
     <AppNavList />
   </v-navigation-drawer>
 
-  <!-- 左侧抽屉 移动端 -->
-  <v-navigation-drawer v-model="rail" temporary @click="rail = false" v-if="$vuetify.display.mobile">
+  <!-- 左侧抽屉 桌面端 -->
+  <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false" v-else>
     <AppNavList />
   </v-navigation-drawer>
 
@@ -58,13 +60,9 @@
 
 <script setup>
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
-const userData = ref({
-  'user': 'Gsoy',
-  'qq': '2754205302',
-  'email': 'GsoyG@icloud.com',
-})
+const userData = ref({})
 const drawer = ref(true) // 折叠侧边栏
 const rail = ref(false) // 显示侧边栏
 const snackbarConfig = ref({ // 提示消息条信息
@@ -74,28 +72,43 @@ const snackbarConfig = ref({ // 提示消息条信息
   'iconColor': '',
 })
 
+onMounted(() => {
+  getUserInfo()
+})
+
 // 显示提示消息条
 function showMessage(text, type) {
   switch (type) {
     case 'success':
       snackbarConfig.value = { show: true, text: text, icon: 'mdi-check-circle', iconColor: 'success' }
-      break;
+      break
     case 'warning':
       snackbarConfig.value = { show: true, text: text, icon: 'mdi-alert-circle', iconColor: 'warning' }
-      break;
+      break
     default:
-      break;
+      break
   }
+}
+
+// 获取用户信息
+async function getUserInfo() {
+  await axios.get('/api/user/info').then(response => {
+    userData.value = response.data
+  }).catch(error => {
+    if (error.response)
+      showMessage('获取用户信息失败 ' + error.response.data, 'warning')
+    else showMessage('获取用户信息出错 ' + error.message, 'warning')
+  })
 }
 
 // 登出
 async function logout() {
   await axios.post('/api/logout').then(response => {
-    window.location.href = "http://127.0.0.1:3000/login";
+    window.location.href = "http://127.0.0.1:3000/login"
   }).catch(error => {
     if (error.response)
       showMessage('退出登入失败 ' + error.response.data, 'warning')
     else showMessage('退出登入出错 ' + error.message, 'warning')
-  });
+  })
 }
 </script>
