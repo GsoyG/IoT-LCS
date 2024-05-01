cbe591b9037bae6353db27760b53ba4f107e26e2
Gsoy <GsoyJanYo@iCloud.com>
2024-05-02 00:32:14 +0800

    优化终端头文件代码结构

--- zstack/source/HAL/HAL_WsLed.h
+++ zstack/source/HAL/HAL_WsLed.h
@@ -2,5 +2,6 @@
 
 // 初始化 WS2812B LED
 void HalWsLedInit(void);
+
 // 设置 LED RGB 颜色
 void HalWsLedSetRGB(uint8 red, uint8 green, uint8 blue);
--- zstack/source/ZCL_SmartLight.h
+++ zstack/source/ZCL_SmartLight.h
@@ -1,16 +1,7 @@
 #ifndef ZCL_SMARTLIGHT_H
 #define ZCL_SMARTLIGHT_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-/*********************************************************************
- * INCLUDES
- */
 #include "zcl.h"
-
 
 // Added to include ZLL Target functionality
 #if defined ( BDB_TL_INITIATOR ) || defined ( BDB_TL_TARGET )
@@ -18,43 +9,24 @@ extern "C"
   #include "bdb_tlCommissioning.h"
 #endif
 
-/*********************************************************************
- * CONSTANTS
- */
-#define SMARTLIGHT_ENDPOINT            8
 // Added to include ZLL Target functionality
-#define SMARTLIGHT_NUM_GRPS            2
-
+#define SMARTLIGHT_NUM_GRPS 2
+#define SMARTLIGHT_ENDPOINT 8
 
 // Application Events
-#define SMARTLIGHT_MAIN_SCREEN_EVT          0x0001
-#define SMARTLIGHT_LEVEL_CTRL_EVT           0x0002
-#define SMARTLIGHT_END_DEVICE_REJOIN_EVT    0x0004  
-  
-/* SMARTLIGHT_TODO: define app events here */
-  
-#define SMARTLIGHT_EVT_1                    0x0008
-/*
-#define SMARTLIGHT_EVT_2                    0x0010
-#define SMARTLIGHT_EVT_3                    0x0020
-*/
+#define SMARTLIGHT_MAIN_SCREEN_EVT       0x0001
+#define SMARTLIGHT_LEVEL_CTRL_EVT        0x0002
+#define SMARTLIGHT_END_DEVICE_REJOIN_EVT 0x0004
+
+#define SMARTLIGHT_EVT_1                 0x0008
+// #define SMARTLIGHT_EVT_2              0x0010
+// #define SMARTLIGHT_EVT_3              0x0020
 
 // Application Display Modes
-#define SMARTLIGHT_MAINMODE      0x00
-#define SMARTLIGHT_HELPMODE      0x01
-  
+#define SMARTLIGHT_MAINMODE 0x00
+#define SMARTLIGHT_HELPMODE 0x01
+
 #define SMARTLIGHT_END_DEVICE_REJOIN_DELAY 10000
-
-/*********************************************************************
- * MACROS
- */
-/*********************************************************************
- * TYPEDEFS
- */
-
-/*********************************************************************
- * VARIABLES
- */
 
 // Added to include ZLL Target functionality
 #if defined ( BDB_TL_INITIATOR ) || defined ( BDB_TL_TARGET )
@@ -76,33 +48,13 @@ extern uint16 zclSmartLight_IdentifyTime;
 extern uint8  zclSmartLight_IdentifyCommissionState;
 
 // SMARTLIGHT_TODO: Declare application specific attributes here
-
-
-/*********************************************************************
- * FUNCTIONS
- */
-
- /*
-  * Initialization for the task
-  */
+// Initialization for the task
 extern void zclSmartLight_Init( byte task_id );
 
-/*
- *  Event Process for the task
- */
+//Event Process for the task
 extern UINT16 zclSmartLight_event_loop( byte task_id, UINT16 events );
 
-/*
- *  Reset all writable attributes to their default values.
- */
+// Reset all writable attributes to their default values
 extern void zclSmartLight_ResetAttributesToDefaultValues(void);
 
-
-/*********************************************************************
-*********************************************************************/
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* ZCL_SMARTLIGHT_H */
+#endif // ZCL_SMARTLIGHT_H
