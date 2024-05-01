7455d2a225a8d7c41a0700e753db7a3821e96587
Gsoy <GsoyJanYo@iCloud.com>
2024-05-01 20:59:27 +0800

    添加终端LED驱动注释

--- zstack/source/HAL/HAL_WsLed.c
+++ zstack/source/HAL/HAL_WsLed.c
@@ -3,20 +3,23 @@
 
 #define LED_SBIT P1_5
 
-// buffersize = 24 bits * 3 bits per bit / 8 bit
+// bufferSize = 24 bits * 3 bits per bit / 8 bit
 #define WS2812_bufferSize 9
 
 uint8 WS2812_buffer[WS2812_bufferSize];
 uint8 WS2812_bit = 0;
 uint16 WS2812_byte = 0;
 
+// 将位存入缓冲区
 void WS2812_StoreBit(uint8 bit);
+// 将颜色存入缓冲区
 void WS2812_StoreColor(uint8 color);
 
 void WS2812_StoreBit(uint8 bit) {
     if (bit) WS2812_buffer[WS2812_byte] |= 1 << WS2812_bit;
     WS2812_bit++;
 
+    // 检测进位
     if (WS2812_bit > 7) {
         WS2812_bit = 0;
         WS2812_byte++;
@@ -42,6 +45,7 @@ void HalWsLedSetRGB(uint8 red, uint8 green, uint8 blue) {
     WS2812_bit = 0;
     WS2812_byte = 0;
 
+    // 清除缓冲区
     for (uint16 i = 0; i < WS2812_bufferSize; i++)
         WS2812_buffer[i] = 0;
 
@@ -51,7 +55,7 @@ void HalWsLedSetRGB(uint8 red, uint8 green, uint8 blue) {
 
     for (uint16 j = 0; j < WS2812_bufferSize; j++) {
         U0DBUF = WS2812_buffer[j];
-        while ((U0CSR & (1 << 0))); // wait until byte is sent
+        while ((U0CSR & (1 << 0))); // 等待发送完成
     }
 }
 
--- zstack/source/HAL/HAL_WsLed.h
+++ zstack/source/HAL/HAL_WsLed.h
@@ -1,4 +1,6 @@
 #include "ZComDef.h"
 
+// 初始化 WS2812B LED
 void HalWsLedInit(void);
+// 设置 LED RGB 颜色
 void HalWsLedSetRGB(uint8 red, uint8 green, uint8 blue);
