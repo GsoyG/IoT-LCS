eea272715182c36815c164a3f2b75481e9c93a0f
Gsoy <GsoyJanYo@iCloud.com>
2024-05-19 23:30:52 +0800

    修复后端日志日期过滤问题

--- services/logger.py
+++ services/logger.py
@@ -15,8 +15,8 @@ def write_log(user, address, type, message):
 
 # 查询日志，参数过滤日志
 def get_logs(start_time = None, end_time = None, user = None, address = None, type = None, message = None):
-    start_query = start_time if start_time is not None else 0
-    end_query = end_time if end_time is not None else time.time()
+    start_query = int(start_time) if (start_time is not None) and (start_time != '') else 0
+    end_query = int(end_time) if (end_time is not None) and (end_time != '') else time.time()
     user_query = user if user is not None else ''
     address_query = address if address is not None else ''
     type_query = type if type is not None else ''
