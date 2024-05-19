612645fe8c56f5731a15ef6f8f18c13f99ad6a4b
Gsoy <GsoyJanYo@iCloud.com>
2024-05-19 22:57:19 +0800

    添加后端日志接口过滤参数

--- controller/log.py
+++ controller/log.py
@@ -14,11 +14,18 @@ async def get_logs(request):
     except KeyError:
         return web.Response(status = 400, text = '参数错误：找不到指定参数')
     
-    logs = logger.get_logs(filter.get('start_time'), filter.get('end_time'), filter.get('user'), filter.get('message'))
+    logs = logger.get_logs(
+        filter.get('start_time'),
+        filter.get('end_time'),
+        filter.get('user'),
+        filter.get('address'),
+        filter.get('type'),
+        filter.get('message')
+    )
     logs_len = len(logs)
 
     if offset >= logs_len:
-        return web.json_response([])
+        return web.json_response({ 'logs': [], 'total': 0 })
     
     # 处理列表长度，最多返回20条
     if count > 20: count = 20
--- services/logger.py
+++ services/logger.py
@@ -14,15 +14,20 @@ def write_log(user, address, type, message):
     })
 
 # 查询日志，参数过滤日志
-def get_logs(start_time = None, end_time = None, user = None, message = None):
+def get_logs(start_time = None, end_time = None, user = None, address = None, type = None, message = None):
     start_query = start_time if start_time is not None else 0
     end_query = end_time if end_time is not None else time.time()
     user_query = user if user is not None else ''
+    address_query = address if address is not None else ''
+    type_query = type if type is not None else ''
     message_query = message if message is not None else ''
+    
     logs = __db.get_items_query(
         (where('timestamp') >= start_query) &
         (where('timestamp') <= end_query) &
         (where('user').search(user_query)) &
+        (where('address').search(address_query)) &
+        (where('type').search(type_query)) &
         (where('message').search(message_query))
     )
     logs.reverse()
