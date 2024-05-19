2fb3368028e35e3cf9cda2c85e2d91f6a073cc3d
Gsoy <GsoyJanYo@iCloud.com>
2024-05-19 23:17:49 +0800

    添加前端日志过滤实现

--- LCS-Frontend/src/pages/LogViewing.vue
+++ LCS-Frontend/src/pages/LogViewing.vue
@@ -3,9 +3,76 @@
     <v-card title="日志查看">
       <v-data-table-server v-model:items-per-page="itemsPerPage" :headers="headers" :items="logList"
         :items-length="totalItems" :loading="loading" loading-text="获取日志列表中......" no-data-text="日志列表为空"
+        :page="currentPage"
         :page-text="`第 ${(currentPage - 1) * itemsPerPage + 1} - ${(currentPage - 1) * itemsPerPage + logList.length} 条 共 ${totalItems} 条`"
-        items-per-page-text="显示数量：" @update:options="loadItems"
-        :items-per-page-options="itemsPerPageOptions"></v-data-table-server>
+        items-per-page-text="显示数量：" @update:options="loadItems" :items-per-page-options="itemsPerPageOptions">
+
+        <!-- 表头，包含过滤器 -->
+        <template v-slot:header.time>
+          <v-btn append-icon="mdi-filter-menu" variant="text" text="日期时间"></v-btn>
+        </template>
+        <template v-slot:header.type>
+          <v-btn variant="text"
+            :append-icon="filterData.type != '' && filterData.type != null ? 'mdi-filter-cog' : 'mdi-filter-menu'"
+            :color="filterData.type != '' && filterData.type != null ? 'indigo-accent-2' : ''">
+            操作类型
+            <v-menu activator="parent" :close-on-content-click="false">
+              <v-card min-width="300">
+                <v-card-text>
+                  <v-select v-model="filterData.type" :items="['用户认证', '设备管理', '定时任务', '情景模式', '账户管理', '日志查看', '系统设置']"
+                    clearable label="类型" variant="outlined" hide-details
+                    @update:model-value="fetchLogList(0)"></v-select>
+                </v-card-text>
+              </v-card>
+            </v-menu>
+          </v-btn>
+        </template>
+        <template v-slot:header.message>
+          <v-btn variant="text"
+            :append-icon="filterData.message != '' && filterData.message != null ? 'mdi-filter-cog' : 'mdi-filter-menu'"
+            :color="filterData.message != '' && filterData.message != null ? 'indigo-accent-2' : ''">
+            操作信息
+            <v-menu activator="parent" :close-on-content-click="false">
+              <v-card min-width="300">
+                <v-card-text>
+                  <v-text-field v-model="filterData.message" label="信息" variant="outlined" hide-details clearable
+                    @update:model-value="fetchLogList(0)"></v-text-field>
+                </v-card-text>
+              </v-card>
+            </v-menu>
+          </v-btn>
+        </template>
+        <template v-slot:header.address>
+          <v-btn variant="text"
+            :append-icon="filterData.address != '' && filterData.address != null ? 'mdi-filter-cog' : 'mdi-filter-menu'"
+            :color="filterData.address != '' && filterData.address != null ? 'indigo-accent-2' : ''">
+            IP地址
+            <v-menu activator="parent" :close-on-content-click="false">
+              <v-card min-width="300">
+                <v-card-text>
+                  <v-text-field v-model="filterData.address" label="信息" variant="outlined" hide-details clearable
+                    @update:model-value="fetchLogList(0)"></v-text-field>
+                </v-card-text>
+              </v-card>
+            </v-menu>
+          </v-btn>
+        </template>
+        <template v-slot:header.user>
+          <v-btn variant="text"
+            :append-icon="filterData.user != '' && filterData.user != null ? 'mdi-filter-cog' : 'mdi-filter-menu'"
+            :color="filterData.user != '' && filterData.user != null ? 'indigo-accent-2' : ''">
+            用户名称
+            <v-menu activator="parent" :close-on-content-click="false">
+              <v-card min-width="300">
+                <v-card-text>
+                  <v-text-field v-model="filterData.user" label="信息" variant="outlined" hide-details clearable
+                    @update:model-value="fetchLogList(0)"></v-text-field>
+                </v-card-text>
+              </v-card>
+            </v-menu>
+          </v-btn>
+        </template>
+      </v-data-table-server>
     </v-card>
 
     <!-- 提示消息条 -->
@@ -20,7 +87,6 @@
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
 
-const itemsPerPage = ref(8)
 const currentPage = ref(1)
 const headers = [
   { key: 'time', sortable: false, width: '15%', minWidth: '180px', title: '日期时间' },
@@ -32,12 +98,19 @@ const headers = [
 const logList = ref([])
 const loading = ref(true)
 const totalItems = ref(0)
-const itemsPerPageOptions = [
+const itemsPerPage = ref(8)
+const itemsPerPageOptions = [ // 每页显示多少条
   { value: 5, title: '5' },
   { value: 10, title: '10' },
   { value: 15, title: '15' },
-  { value: 20, title: '20' }
+  { value: 20, title: '20' },
 ]
+const filterData = ref({
+  user: '',
+  address: '',
+  type: '',
+  message: '',
+})
 
 const snackbarConfig = ref({ // 提示消息条信息
   'show': false,
@@ -47,7 +120,7 @@ const snackbarConfig = ref({ // 提示消息条信息
 })
 
 onMounted(() => {
-  fetchLogList(0, itemsPerPage.value, {})
+  fetchLogList(0)
 })
 
 // 显示提示消息条
@@ -65,20 +138,21 @@ function showMessage(text, type) {
 }
 
 // 获取日志列表
-async function fetchLogList(offset, count, filter) {
+async function fetchLogList(offset) {
   loading.value = true
+  currentPage.value = offset / itemsPerPage.value + 1 // 当前页码
 
   await axios.get('/api/log/logs', {
     params: {
       offset: offset,
-      count: count,
-      filter: JSON.stringify(filter)
+      count: itemsPerPage.value,
+      filter: JSON.stringify(filterData.value)
     }
   }).then(response => {
     logList.value = response.data.logs
     totalItems.value = response.data.total
 
-    logList.value.forEach(log => {
+    logList.value.forEach(log => { // 格式化时间
       const time = new Date(log.timestamp * 1000)
       log.time = time.toLocaleString()
     })
@@ -92,7 +166,6 @@ async function fetchLogList(offset, count, filter) {
 
 // 更新列表显示
 function loadItems({ page, itemsPerPage, sortBy }) {
-  currentPage.value = page
-  fetchLogList((page - 1) * itemsPerPage, itemsPerPage, {})
+  fetchLogList((page - 1) * itemsPerPage, itemsPerPage)
 }
 </script>
