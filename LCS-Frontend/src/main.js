93e9d81b927342da231f894f06b5443f02554055
Gsoy <GsoyJanYo@iCloud.com>
2024-05-28 02:00:50 +0800

    简化前端登录页面模板入口

--- LCS-Frontend/src/main.js
+++ LCS-Frontend/src/main.js
@@ -1,12 +1,21 @@
 import { registerPlugins } from '@/plugins'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Login from './Login.vue'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:5000'
 axios.defaults.timeout = 3000
 axios.defaults.withCredentials = true
 
-const app = createApp(App)
-registerPlugins(app)
-app.mount('#app')
+if (document.getElementById('app')) {
+    const app = createApp(App)
+    registerPlugins(app)
+    app.mount('#app')
+}
+else if (document.getElementById('login-app')) {
+    const app = createApp(Login)
+    registerPlugins(app)
+    app.mount('#login-app')
+}
+
